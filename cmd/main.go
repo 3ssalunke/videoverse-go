d04127fa7df2bbe794d11111731ca3828a3d05e5
3ssalunke <surajsalunke1276@gmail.com>
2025-01-28 01:27:27 +0530

    280125: add gin

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "github.com/3ssalunke/videoverse/db"
+import (
+	"github.com/3ssalunke/videoverse/db"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	db.Init()
+
+	r := gin.Default()
+
+	r.Run(":8080")
 }
